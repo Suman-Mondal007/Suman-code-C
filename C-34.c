66e2f4d0651a242582e9e8ace50c31c4768ab350
Suman Mondal <145131739+Suman-Mondal007@users.noreply.github.com>
2023-10-04 00:07:31 +0530

    Create C-34.c

---
+++ C-34.c
@@ -0,0 +1,7 @@
+//Write a program in c to demonstrate shift left ,shift right operator.
+#include <stdio.h>
+int main (){
+    int a=10;
+    printf("%d\n",a<<1);//shift left
+    printf("%d",a>>1);//shift right
+}
