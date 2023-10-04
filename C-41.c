babe2c3ba94b8beec08b3a0b23632a752650f2b8
Suman Mondal <145131739+Suman-Mondal007@users.noreply.github.com>
2023-10-04 23:07:27 +0530

    Create C-41.c

---
+++ C-41.c
@@ -0,0 +1,22 @@
+/* Write a  program in cto print the following febonachis structure
+*
+**
+***
+*****
+*********/
+#include<stdio.h>
+int main(){
+    int limit,temp1=1,temp2=1,backup=0;
+    printf("Enter the limit:\n");
+    scanf("%d",&limit);
+    
+    for (int i=0 ;i<=limit;i++){
+        for (int j=0;j<temp1;j++){
+            printf("*");
+        }
+        printf("\n");
+        backup=temp2;
+        temp2=temp1+temp2;
+        temp1=backup;
+    }
+}
