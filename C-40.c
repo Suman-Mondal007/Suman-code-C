bfb9b9a8f0b0d1296d1218172d3f3619a4253bd4
Suman Mondal <145131739+Suman-Mondal007@users.noreply.github.com>
2023-10-04 11:36:05 +0530

    Create C-40.c

---
+++ C-40.c
@@ -0,0 +1,14 @@
+//multiplication of number within a specific range using for loop
+#include<stdio.h> 
+  
+ int main(){ 
+     int range; 
+     printf("Enter the range:\n"); 
+     scanf("%d",&range); 
+     for(int i=1;i<=range;i++){ 
+         for(int j=1;j<=10;j++){ 
+             printf("%d*%d=%d\n",i,j,i*j); 
+         } 
+         printf("___________________\n"); 
+     } 
+ }
