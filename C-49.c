8baeca11ab583579d80ca53631e5b0890a6ceb31
Suman Mondal <145131739+Suman-Mondal007@users.noreply.github.com>
2023-10-10 23:15:47 +0530

    Create C-49.c

---
+++ C-49.c
@@ -0,0 +1,16 @@
+//Write a program in c  to display the name of my five best friend using function.
+#include <stdio.h>
+void displayFriends() {
+    printf("My five best friends are:\n");
+    printf("1. Friend Sounak \n");
+    printf("2. Friend Debo \n");
+    printf("3. Friend sayan;
+    printf("4. Friend Ram\n");
+    printf("5. Friend Shyam\n");
+}
+
+int main() {
+    
+    displayFriends();
+
+}
